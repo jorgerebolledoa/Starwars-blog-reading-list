f7d7c9b3aecc5bf81782cb19809fd0200fdb9235
pedroamenga <pedroam92@gmail.com>
2022-05-02 21:41:37 +0000

    card y single person creado

---
+++ src/js/views/Singleperson.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect, useContext } from "react";
+import PropTypes from "prop-types";
+import { Context } from "../store/appContext";
+
+export const Single = props => {
+	const { store, actions } = useContext(Context);
+	return (
+	<>
+	
+	</>
+	);
+};
