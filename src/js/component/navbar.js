4a07a2b9858d6910600006e77f6afb6e49ec2bf8
jorgerebolledoa <aziel.az18@gmail.com>
2022-05-02 20:50:49 +0000

    sw

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar navbar-light bg-dark mb-3 text-warning">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1 text-warning m-2">Star Wars</span>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+					<button className="btn btn-warning m-2">Check the Context in action</button>
 				</Link>
 			</div>
 		</nav>
