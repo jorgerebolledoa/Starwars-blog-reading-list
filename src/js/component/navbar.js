d407e91267b8af3933497ff712b998ff3b1ea4b1
jorgerebolledoa <aziel.az18@gmail.com>
2022-06-08 14:32:52 -0400

    mapeo people sin orden

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,21 +2,24 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../../styles/nav.css"
 
+
 export const Navbar = () => {
 	return (
-		<nav className="navbar  mb-3 	text-secondary">
+		<nav className="navbar  mb-3 text-secondary">
 			<div className="container_icon ">
 				<a href="https://www.facebook.com/"><i className="fab fa-facebook-f"></i></a>				
 				<a href="https://twitter.com/home"><i className="fab fa-twitter"></i></a>
 				<a href="https://www.instagram.com/"><i className="fab fa-instagram"></i></a>
+				<a href="https://www.youtube.com/"><i className="fa-brands fa-youtube"></i></a>
+				
 			</div>
 			<Link to="/">
-				<img className="float-center" src="https://seeklogo.com/images/S/Star_Wars-logo-B06952A462-seeklogo.com.png">
+				<img className="float-center img_st" src="https://seeklogo.com/images/S/Star_Wars-logo-B06952A462-seeklogo.com.png">
 				</img>
 			</Link>
-			<form class="d-flex" role="search">
-				<input class="form-control me-2 bg-dark" type="search" placeholder="Search" aria-label="Search" />
-				<button class="btn btn-outline-warning" type="submit">Search</button>
+			<form className="d-flex" role="search">
+				<input className="form-control me-2 bg-dark" type="search" placeholder="Search" aria-label="Search" />
+				<button className="btn btn-outline-warning" type="submit">Search</button>
 			</form>
 		</nav>
 	);
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,51 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
+import React, { useContext, useState, useEffect } from "react";
+import Card from "../component/Card";
+import "../../styles/home.css"
+export const Home = () => {
+	const [lista, setlista] = useState([]);
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+	const urlApi = "https://www.swapi.tech/api/people"
+	useEffect(() => {
+		getTask(urlApi);
+	}, []);
+
+	const getTask = (url) => {
+		fetch(url)
+			.then((Response) => Response.json())
+			.then((data) => {
+				console.log(data);
+				setlista(data);
+			})
+			.catch((error) => console.log(error));
+	};
+
+	let peopleList = lista.results?.map((tastk, index) => {
+		return (
+			<div key={tastk.uid} className="container ">
+				<div class="columms align-items-center">
+					<div class="col-3">
+						<Card
+							img="https://seeklogo.com/images/S/Star_Wars-logo-B06952A462-seeklogo.com.png"
+							title={tastk.name}
+							text={tastk.description_1}
+							linkbtn={"/producto/" + tastk.id}
+							textbtn="learn more"
+						/>
+					</div>
+				</div>
+			</div>
+
+		);
+	});
+
+	return (
+		<>
+			<h1 className="title text-center">Characters of Star Wars</h1>
+			<div className="planetsContainer animate__animated animate__fadeInRightBig">
+				{peopleList}
+			</div>
+
+
+		</>
+	);
+};
