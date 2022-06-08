d472a240491d111dfdfabda4fa2798bfa5ec6942
jorgerebolledoa <aziel.az18@gmail.com>
2022-06-08 13:43:39 -0400

    cambios nav

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/nav.css"
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-dark mb-3 text-warning">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1 text-warning m-2">Star Wars</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-warning m-2">Check the Context in action</button>
-				</Link>
+		<nav className="navbar  mb-3 	text-secondary">
+			<div className="container_icon ">
+				<a href="https://www.facebook.com/"><i className="fab fa-facebook-f"></i></a>				
+				<a href="https://twitter.com/home"><i className="fab fa-twitter"></i></a>
+				<a href="https://www.instagram.com/"><i className="fab fa-instagram"></i></a>
 			</div>
+			<Link to="/">
+				<img className="float-center" src="https://seeklogo.com/images/S/Star_Wars-logo-B06952A462-seeklogo.com.png">
+				</img>
+			</Link>
+			<form class="d-flex" role="search">
+				<input class="form-control me-2 bg-dark" type="search" placeholder="Search" aria-label="Search" />
+				<button class="btn btn-outline-warning" type="submit">Search</button>
+			</form>
 		</nav>
 	);
 };
